99038d825068295e7e95c8ef09547f9a333c0b63
Jacob Hale <Jchale23@byui.edu>
2026-04-06 15:27:12 -0600

    phase 6 of admin stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { AdminRoute } from './components/admin/AdminRoute';
 import { AppLayout } from './components/AppLayout';
+import { AdminDashboardPage } from './pages/AdminDashboardPage';
+import { CaseloadInventoryPage } from './pages/CaseloadInventoryPage';
 import { HomePage } from './pages/HomePage';
 import { ImpactDashboardPage } from './pages/ImpactDashboardPage';
 import { LoginPage } from './pages/LoginPage';
 import { PrivacyPolicyPage } from './pages/PrivacyPolicyPage';
+import { ProcessRecordingPage } from './pages/ProcessRecordingPage';
 
 function App() {
   return (
@@ -13,6 +17,11 @@ function App() {
         <Route path="login" element={<LoginPage />} />
         <Route path="impact-dashboard" element={<ImpactDashboardPage />} />
         <Route path="privacy" element={<PrivacyPolicyPage />} />
+        <Route path="admin" element={<AdminRoute />}>
+          <Route index element={<AdminDashboardPage />} />
+          <Route path="caseload" element={<CaseloadInventoryPage />} />
+          <Route path="process-recordings" element={<ProcessRecordingPage />} />
+        </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
